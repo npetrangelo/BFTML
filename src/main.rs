b2569aba3ee9fba135354afb145e407c43067060
Nathan Petrangelo <nathan@petrangelo.org>
2024-10-29 22:13:23 -0700

    Added xml parser and file to test with

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::{fs::File, io::BufReader};
 
 use iced::widget::{button, column, text, Column};
+use xml::{reader::XmlEvent, EventReader};
 
 #[derive(Default)]
 struct Counter {
@@ -34,6 +35,34 @@ enum Message {
     Decrement,
 }
 
+fn xml_file() -> std::io::Result<()> {
+    let file = File::open("file.xml")?;
+    let file = BufReader::new(file); // Buffering is important for performance
+
+    let parser = EventReader::new(file);
+    let mut depth = 0;
+    for e in parser {
+        match e {
+            Ok(XmlEvent::StartElement { name, .. }) => {
+                println!("{:spaces$}+{name}", "", spaces = depth * 2);
+                depth += 1;
+            }
+            Ok(XmlEvent::EndElement { name }) => {
+                depth -= 1;
+                println!("{:spaces$}-{name}", "", spaces = depth * 2);
+            }
+            Err(e) => {
+                eprintln!("Error: {e}");
+                break;
+            }
+            // There's more: https://docs.rs/xml-rs/latest/xml/reader/enum.XmlEvent.html
+            _ => {}
+        }
+    }
+    Ok(())
+}
+
 fn main() -> iced::Result {
+    xml_file().unwrap();
     iced::run("A cool counter", Counter::update, Counter::view)
 }
