9fa0d1ac66ad7a87a53e80228dc780c700ba697f
Nathan Petrangelo <nathan@petrangelo.org>
2024-11-10 12:03:11 -0800

    Successfully parses children

--- src/elements/parser.rs
+++ src/elements/parser.rs
@@ -3,14 +3,6 @@ use std::str::FromStr;
 use indexmap::IndexMap;
 use winnow::{ascii::{alphanumeric1, multispace0}, combinator::repeat, error::{ContextError, ErrMode}, PResult, Parser};
 
-#[derive(Debug, PartialEq)]
-enum Token {
-    TagOpen,
-    TagClose,
-    TagElement(String),
-    Attribute((String, String))
-}
-
 fn parse_attribute<'s>(s: &mut &'s str) -> PResult<(&'s str, &'s str)> {
     let _ = multispace0.parse_next(s)?;
     let key = alphanumeric1.parse_next(s)?;
@@ -26,21 +18,49 @@ fn parse_attributes<'s>(s: &mut &'s str) -> PResult<IndexMap<String, String>> {
     }).parse_next(s)
 }
 
-#[derive(Debug, PartialEq)]
+fn parse_children<'s>(s: &mut &'s str) -> PResult<Vec<Tag>> {
+    repeat(0.., parse_tag).fold(Vec::new, |mut acc: Vec<Tag>, item| {
+        acc.push(item);
+        acc
+    }).parse_next(s)
+}
+
+#[derive(Clone, Debug, PartialEq)]
 pub struct Tag {
     pub name: String,
     pub attributes: IndexMap<String, String>,
     pub children: Vec<Tag>
 }
 
+impl Tag {
+    pub fn new(name: &str) -> Self {
+        Tag { name: name.into(), attributes: IndexMap::new(), children: vec![] }
+    }
+
+    pub fn with(&mut self, key: &str, value: &str) {
+        self.attributes.insert(key.into(), value.into());
+    }
+
+    pub fn child(&mut self, tag: Tag) {
+        self.children.push(tag);
+    }
+}
+
 fn parse_tag<'a>(s: &mut &'a str) -> PResult<Tag> {
+    let _ = multispace0.parse_next(s)?;
     let _ = "<".parse_next(s)?;
-    let name = alphanumeric1.parse_next(s)?.into();
+    let name: String = alphanumeric1.parse_next(s)?.into();
 
     let attributes = parse_attributes.parse_next(s)?;
 
     let _ = ">".parse_next(s)?;
-    let children = vec![];
+
+    let children = parse_children.parse_next(s)?;
+
+    let _ = "</".parse_next(s)?;
+    let _ = name.as_str().parse_next(s)?;
+    let _ = ">".parse_next(s)?;
+
     Ok(Tag { name, attributes, children })
 }
 
@@ -61,12 +81,13 @@ mod test {
     use crate::elements::parser::Tag;
 
     use super::parse_attribute;
+    use super::parse_children;
     use super::parse_tag;
 
     #[test]
     fn test_parse_tag() {
         let expected = Tag { name: "button".into(), attributes: IndexMap::new(), children: vec![] };
-        assert_eq!(Ok(expected), "<button>".parse());
+        assert_eq!(Ok(expected), "<button></button>".parse());
     }
 
     #[test]
@@ -97,7 +118,7 @@ mod test {
 
     #[test]
     fn test_parse_tag_attr() {
-        let mut input = "<button foo=bar>";
+        let mut input = "<button foo=bar></button>";
         let parsed = parse_tag.parse_next(&mut input);
         assert_eq!(input, "");
         assert!(parsed.is_ok());
@@ -107,6 +128,30 @@ mod test {
         attributes.insert("foo".into(), "bar".into());
         let expected = Tag { name: "button".into(), attributes, children: vec![] };
         assert_eq!(tag, expected);
+    }
+
+    #[test]
+    fn test_parse_children() {
+        let parsed = parse_children.parse_next(&mut "<foo></foo><bar></bar>");
+        let expected = vec![Tag::new("foo"), Tag::new("bar")];
+        assert_eq!(expected, parsed.unwrap())
+    }
+
+    #[test]
+    fn test_parse_tag_children() {
+        let parsed = parse_tag.parse_next(&mut "<foo><bar></bar></foo>");
+        let mut expected = Tag::new("foo");
+        expected.child(Tag::new("bar"));
+        assert_eq!(expected, parsed.unwrap())
+    }
+
+    #[test]
+    fn test_altogether() {
+        let parsed = parse_tag.parse_next(&mut "<button foo=bar><foo></foo></button>");
+        let mut expected = Tag::new("button");
+        expected.with("foo", "bar");
+        expected.child(Tag::new("foo"));
+        assert_eq!(expected, parsed.unwrap())
     }
 
     #[test]
